446e10a47826ac0469ccc364339231be06a76747
Philip Rehberger <me@philiprehberger.com>
2022-05-10 23:55:49 -0600

    Standardize RuboCop config and auto-correct offenses

--- philiprehberger-config_kit.gemspec
+++ philiprehberger-config_kit.gemspec
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
-require_relative "lib/philiprehberger/config_kit/version"
+require_relative 'lib/philiprehberger/config_kit/version'
 
 Gem::Specification.new do |spec|
-  spec.name = "philiprehberger-config_kit"
+  spec.name = 'philiprehberger-config_kit'
   spec.version = Philiprehberger::ConfigKit::VERSION
-  spec.authors = ["Philip Rehberger"]
-  spec.email = ["me@philiprehberger.com"]
+  spec.authors = ['Philip Rehberger']
+  spec.email = ['me@philiprehberger.com']
 
-  spec.summary = "Layered configuration with YAML, ENV, and defaults"
-  spec.description = "A zero-dependency Ruby gem for layered configuration resolution. " \
-                     "Define typed config keys with defaults, load from YAML files, " \
-                     "and override with environment variables."
-  spec.homepage = "https://github.com/philiprehberger/rb-config-kit"
-  spec.license = "MIT"
+  spec.summary = 'Layered configuration with YAML, ENV, and defaults'
+  spec.description = 'A zero-dependency Ruby gem for layered configuration resolution. ' \
+                     'Define typed config keys with defaults, load from YAML files, ' \
+                     'and override with environment variables.'
+  spec.homepage = 'https://github.com/philiprehberger/rb-config-kit'
+  spec.license = 'MIT'
 
-  spec.required_ruby_version = ">= 3.1.0"
+  spec.required_ruby_version = '>= 3.1.0'
 
-  spec.metadata["homepage_uri"]    = spec.homepage
-  spec.metadata["source_code_uri"] = spec.homepage
-  spec.metadata["changelog_uri"]   = "#{spec.homepage}/blob/main/CHANGELOG.md"
-  spec.metadata["bug_tracker_uri"]       = "#{spec.homepage}/issues"
-  spec.metadata["rubygems_mfa_required"] = "true"
+  spec.metadata['homepage_uri']    = spec.homepage
+  spec.metadata['source_code_uri'] = spec.homepage
+  spec.metadata['changelog_uri']   = "#{spec.homepage}/blob/main/CHANGELOG.md"
+  spec.metadata['bug_tracker_uri']       = "#{spec.homepage}/issues"
+  spec.metadata['rubygems_mfa_required'] = 'true'
 
-  spec.files = Dir["lib/**/*.rb", "LICENSE", "README.md", "CHANGELOG.md"]
-  spec.require_paths = ["lib"]
+  spec.files = Dir['lib/**/*.rb', 'LICENSE', 'README.md', 'CHANGELOG.md']
+  spec.require_paths = ['lib']
 end
--- spec/philiprehberger/config_kit_spec.rb
+++ spec/philiprehberger/config_kit_spec.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-require "spec_helper"
-require "tempfile"
-require "yaml"
+require 'spec_helper'
+require 'tempfile'
+require 'yaml'
 
 RSpec.describe Philiprehberger::ConfigKit do
-  describe ".define" do
-    it "returns a Store instance" do
+  describe '.define' do
+    it 'returns a Store instance' do
       config = described_class.define do
-        string :name, default: "test"
+        string :name, default: 'test'
       end
 
       expect(config).to be_a(Philiprehberger::ConfigKit::Store)
     end
   end
 
-  describe "defaults" do
-    it "resolves string defaults" do
+  describe 'defaults' do
+    it 'resolves string defaults' do
       config = described_class.define(env: {}) do
-        string :app_name, default: "my-app"
+        string :app_name, default: 'my-app'
       end
 
-      expect(config[:app_name]).to eq("my-app")
+      expect(config[:app_name]).to eq('my-app')
     end
 
-    it "resolves integer defaults" do
+    it 'resolves integer defaults' do
       config = described_class.define(env: {}) do
         integer :port, default: 3000
       end
@@ -32,7 +32,7 @@ RSpec.describe Philiprehberger::ConfigKit do
       expect(config[:port]).to eq(3000)
     end
 
-    it "resolves boolean defaults" do
+    it 'resolves boolean defaults' do
       config = described_class.define(env: {}) do
         boolean :debug, default: false
       end
@@ -40,7 +40,7 @@ RSpec.describe Philiprehberger::ConfigKit do
       expect(config[:debug]).to eq(false)
     end
 
-    it "resolves float defaults" do
+    it 'resolves float defaults' do
       config = described_class.define(env: {}) do
         float :rate, default: 0.75
       end
@@ -48,7 +48,7 @@ RSpec.describe Philiprehberger::ConfigKit do
       expect(config[:rate]).to eq(0.75)
     end
 
-    it "returns nil for keys with no default" do
+    it 'returns nil for keys with no default' do
       config = described_class.define(env: {}) do
         string :optional
       end
@@ -57,68 +57,68 @@ RSpec.describe Philiprehberger::ConfigKit do
     end
   end
 
-  describe "YAML loading" do
-    it "loads values from a YAML file" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("app_name" => "yaml-app", "port" => 8080))
+  describe 'YAML loading' do
+    it 'loads values from a YAML file' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('app_name' => 'yaml-app', 'port' => 8080))
       yaml_file.close
 
       config = described_class.define(yaml: yaml_file.path, env: {}) do
-        string :app_name, default: "default-app"
+        string :app_name, default: 'default-app'
         integer :port, default: 3000
       end
 
-      expect(config[:app_name]).to eq("yaml-app")
+      expect(config[:app_name]).to eq('yaml-app')
       expect(config[:port]).to eq(8080)
     ensure
       yaml_file&.unlink
     end
 
-    it "falls back to defaults when YAML key is missing" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("app_name" => "yaml-app"))
+    it 'falls back to defaults when YAML key is missing' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('app_name' => 'yaml-app'))
       yaml_file.close
 
       config = described_class.define(yaml: yaml_file.path, env: {}) do
-        string :app_name, default: "default-app"
+        string :app_name, default: 'default-app'
         integer :port, default: 3000
       end
 
-      expect(config[:app_name]).to eq("yaml-app")
+      expect(config[:app_name]).to eq('yaml-app')
       expect(config[:port]).to eq(3000)
     ensure
       yaml_file&.unlink
     end
 
-    it "handles missing YAML file gracefully" do
-      config = described_class.define(yaml: "/nonexistent/config.yml", env: {}) do
-        string :app_name, default: "default-app"
+    it 'handles missing YAML file gracefully' do
+      config = described_class.define(yaml: '/nonexistent/config.yml', env: {}) do
+        string :app_name, default: 'default-app'
       end
 
-      expect(config[:app_name]).to eq("default-app")
+      expect(config[:app_name]).to eq('default-app')
     end
   end
 
-  describe "ENV overrides" do
-    it "overrides defaults with ENV values" do
-      env = { "APP_PORT" => "9090" }
+  describe 'ENV overrides' do
+    it 'overrides defaults with ENV values' do
+      env = { 'APP_PORT' => '9090' }
 
       config = described_class.define(env: env) do
-        integer :port, default: 3000, env: "APP_PORT"
+        integer :port, default: 3000, env: 'APP_PORT'
       end
 
       expect(config[:port]).to eq(9090)
     end
 
-    it "overrides YAML values with ENV values" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("port" => 8080))
+    it 'overrides YAML values with ENV values' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('port' => 8080))
       yaml_file.close
 
-      env = { "APP_PORT" => "9090" }
+      env = { 'APP_PORT' => '9090' }
 
       config = described_class.define(yaml: yaml_file.path, env: env) do
-        integer :port, default: 3000, env: "APP_PORT"
+        integer :port, default: 3000, env: 'APP_PORT'
       end
 
       expect(config[:port]).to eq(9090)
@@ -126,37 +126,37 @@ RSpec.describe Philiprehberger::ConfigKit do
       yaml_file&.unlink
     end
 
-    it "ignores ENV when the variable is not set" do
+    it 'ignores ENV when the variable is not set' do
       config = described_class.define(env: {}) do
-        string :name, default: "fallback", env: "APP_NAME"
+        string :name, default: 'fallback', env: 'APP_NAME'
       end
 
-      expect(config[:name]).to eq("fallback")
+      expect(config[:name]).to eq('fallback')
     end
   end
 
-  describe "type casting" do
-    it "casts string from ENV" do
-      config = described_class.define(env: { "VAL" => "hello" }) do
-        string :val, env: "VAL"
+  describe 'type casting' do
+    it 'casts string from ENV' do
+      config = described_class.define(env: { 'VAL' => 'hello' }) do
+        string :val, env: 'VAL'
       end
 
-      expect(config[:val]).to eq("hello")
+      expect(config[:val]).to eq('hello')
       expect(config[:val]).to be_a(String)
     end
 
-    it "casts integer from ENV" do
-      config = described_class.define(env: { "VAL" => "42" }) do
-        integer :val, env: "VAL"
+    it 'casts integer from ENV' do
+      config = described_class.define(env: { 'VAL' => '42' }) do
+        integer :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(42)
       expect(config[:val]).to be_a(Integer)
     end
 
-    it "casts float from ENV" do
-      config = described_class.define(env: { "VAL" => "3.14" }) do
-        float :val, env: "VAL"
+    it 'casts float from ENV' do
+      config = described_class.define(env: { 'VAL' => '3.14' }) do
+        float :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(3.14)
@@ -164,76 +164,76 @@ RSpec.describe Philiprehberger::ConfigKit do
     end
 
     it "casts boolean 'true' from ENV" do
-      config = described_class.define(env: { "VAL" => "true" }) do
-        boolean :val, env: "VAL"
+      config = described_class.define(env: { 'VAL' => 'true' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(true)
     end
 
     it "casts boolean 'false' from ENV" do
-      config = described_class.define(env: { "VAL" => "false" }) do
-        boolean :val, env: "VAL"
+      config = described_class.define(env: { 'VAL' => 'false' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(false)
     end
 
     it "casts boolean '1' as true" do
-      config = described_class.define(env: { "VAL" => "1" }) do
-        boolean :val, env: "VAL"
+      config = described_class.define(env: { 'VAL' => '1' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(true)
     end
 
     it "casts boolean '0' as false" do
-      config = described_class.define(env: { "VAL" => "0" }) do
-        boolean :val, env: "VAL"
+      config = described_class.define(env: { 'VAL' => '0' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(false)
     end
   end
 
-  describe "Store" do
+  describe 'Store' do
     subject(:config) do
       described_class.define(env: {}) do
-        string :name, default: "app"
+        string :name, default: 'app'
         integer :port, default: 3000
       end
     end
 
-    it "supports #get" do
-      expect(config.get(:name)).to eq("app")
+    it 'supports #get' do
+      expect(config.get(:name)).to eq('app')
     end
 
-    it "supports #[] accessor" do
+    it 'supports #[] accessor' do
       expect(config[:port]).to eq(3000)
     end
 
-    it "exports all values with #to_h" do
-      expect(config.to_h).to eq({ name: "app", port: 3000 })
+    it 'exports all values with #to_h' do
+      expect(config.to_h).to eq({ name: 'app', port: 3000 })
     end
 
-    it "lists all keys" do
+    it 'lists all keys' do
       expect(config.keys).to contain_exactly(:name, :port)
     end
 
-    it "checks key existence" do
+    it 'checks key existence' do
       expect(config.key?(:name)).to be(true)
       expect(config.key?(:missing)).to be(false)
     end
 
-    it "is frozen after creation" do
+    it 'is frozen after creation' do
       expect(config).to be_frozen
     end
 
-    it "returns nil for undefined keys" do
+    it 'returns nil for undefined keys' do
       expect(config[:nonexistent]).to be_nil
     end
 
-    it "to_h returns independent copy" do
+    it 'to_h returns independent copy' do
       h1 = config.to_h
       h2 = config.to_h
       expect(h1).not_to be(h2)
@@ -241,34 +241,34 @@ RSpec.describe Philiprehberger::ConfigKit do
     end
   end
 
-  describe "type casting" do
+  describe 'type casting' do
     it "casts boolean 'yes' as true" do
-      config = described_class.define(env: { "VAL" => "yes" }) do
-        boolean :val, env: "VAL"
+      config = described_class.define(env: { 'VAL' => 'yes' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(true)
     end
 
     it "casts boolean 'no' as false" do
-      config = described_class.define(env: { "VAL" => "no" }) do
-        boolean :val, env: "VAL"
+      config = described_class.define(env: { 'VAL' => 'no' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(false)
     end
 
-    it "casts non-standard boolean values with double-bang" do
-      config = described_class.define(env: { "VAL" => "anything" }) do
-        boolean :val, env: "VAL"
+    it 'casts non-standard boolean values with double-bang' do
+      config = described_class.define(env: { 'VAL' => 'anything' }) do
+        boolean :val, env: 'VAL'
       end
 
       expect(config[:val]).to eq(true)
     end
 
-    it "casts integer from YAML" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("count" => 42))
+    it 'casts integer from YAML' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('count' => 42))
       yaml_file.close
 
       config = described_class.define(yaml: yaml_file.path, env: {}) do
@@ -280,9 +280,9 @@ RSpec.describe Philiprehberger::ConfigKit do
       yaml_file&.unlink
     end
 
-    it "casts float from YAML" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("rate" => 0.5))
+    it 'casts float from YAML' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('rate' => 0.5))
       yaml_file.close
 
       config = described_class.define(yaml: yaml_file.path, env: {}) do
@@ -295,64 +295,64 @@ RSpec.describe Philiprehberger::ConfigKit do
     end
   end
 
-  describe "layer precedence" do
-    it "ENV > YAML > defaults (full stack)" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("name" => "yaml-val"))
+  describe 'layer precedence' do
+    it 'ENV > YAML > defaults (full stack)' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('name' => 'yaml-val'))
       yaml_file.close
 
-      config = described_class.define(yaml: yaml_file.path, env: { "APP_NAME" => "env-val" }) do
-        string :name, default: "default-val", env: "APP_NAME"
+      config = described_class.define(yaml: yaml_file.path, env: { 'APP_NAME' => 'env-val' }) do
+        string :name, default: 'default-val', env: 'APP_NAME'
       end
 
-      expect(config[:name]).to eq("env-val")
+      expect(config[:name]).to eq('env-val')
     ensure
       yaml_file&.unlink
     end
 
-    it "YAML > defaults when ENV not set" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("name" => "yaml-val"))
+    it 'YAML > defaults when ENV not set' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('name' => 'yaml-val'))
       yaml_file.close
 
       config = described_class.define(yaml: yaml_file.path, env: {}) do
-        string :name, default: "default-val", env: "APP_NAME"
+        string :name, default: 'default-val', env: 'APP_NAME'
       end
 
-      expect(config[:name]).to eq("yaml-val")
+      expect(config[:name]).to eq('yaml-val')
     ensure
       yaml_file&.unlink
     end
   end
 
-  describe "multiple definitions" do
-    it "supports many keys of mixed types" do
+  describe 'multiple definitions' do
+    it 'supports many keys of mixed types' do
       config = described_class.define(env: {}) do
-        string :name, default: "app"
+        string :name, default: 'app'
         integer :port, default: 3000
         boolean :debug, default: false
         float :rate, default: 1.5
       end
 
       expect(config.keys).to contain_exactly(:name, :port, :debug, :rate)
-      expect(config[:name]).to eq("app")
+      expect(config[:name]).to eq('app')
       expect(config[:port]).to eq(3000)
       expect(config[:debug]).to eq(false)
       expect(config[:rate]).to eq(1.5)
     end
   end
 
-  describe "YAML edge cases" do
-    it "handles YAML file with non-hash root" do
-      yaml_file = Tempfile.new(["config", ".yml"])
-      yaml_file.write(YAML.dump("just a string"))
+  describe 'YAML edge cases' do
+    it 'handles YAML file with non-hash root' do
+      yaml_file = Tempfile.new(['config', '.yml'])
+      yaml_file.write(YAML.dump('just a string'))
       yaml_file.close
 
       config = described_class.define(yaml: yaml_file.path, env: {}) do
-        string :name, default: "fallback"
+        string :name, default: 'fallback'
       end
 
-      expect(config[:name]).to eq("fallback")
+      expect(config[:name]).to eq('fallback')
     ensure
       yaml_file&.unlink
     end
